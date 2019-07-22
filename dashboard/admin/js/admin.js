f701a390b8a70903eff488d334c58aa07cb47d08
pankajgouraw <pgouraw@gmail.com>
2019-07-22 17:00:14 +0530

    admin pages development

--- dashboard/admin/js/admin.js
+++ dashboard/admin/js/admin.js
@@ -1,4 +1,25 @@
    $('nav').load('../../components/navbar/navbar.html');
    $('header').load('header/header.html');
    $('footer').load('../../components/footer/footer.html');
+   
+   
+   $(document).ready(function(){
+      $('.myClass').click(function(){
+        $('.myClassDetail').slideToggle();  
+      })
+      
+      $('.adminAccountInfo').click(function(){
+        $('.adminAccount').slideToggle();  
+      })
+      
+      $('.changPass').click(function(){
+        $('.loginPop').fadeIn();  
+      })
+      
+      $('.crossChangePass').click(function(){
+        $('.loginPop').fadeOut();  
+      })
+      
+      
+   })
 
